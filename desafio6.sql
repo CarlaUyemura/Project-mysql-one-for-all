55848cd2ed2e8765f2e8ef3c40ec21ef503434c8
Carla Uyemura <carla.uyemura@gmail.com>
2022-09-28 15:49:09 -0300

    Ajuste

--- desafio6.sql
+++ desafio6.sql
@@ -1,3 +1,9 @@
-SELECT MIN(price) AS faturamento_minimo, MAX(price) AS faturamento_maximo, ROUND(AVG(price), 2) AS faturamento_medio, SUM(price) AS faturamento_total
-FROM SpotifyClone.plans p
-INNER JOIN users u ON u.id_plan = p.id_plan;
+SELECT
+    MIN(price) AS faturamento_minimo,
+    MAX(price) AS faturamento_maximo,
+    ROUND(AVG(price), 2) AS faturamento_medio,
+    SUM(price) AS faturamento_total
+FROM
+    SpotifyClone.plans p
+        INNER JOIN
+    users u ON u.id_plan = p.id_plan;
